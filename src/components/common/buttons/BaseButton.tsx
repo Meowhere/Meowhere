18c4f71c1ee6274291dfa878f1f80bd3e81ce19f
tansxx <159680008+tansxx@users.noreply.github.com>
2025-05-29 00:56:13 +0900

    ✨Feat: BaseButton 컴포넌트 구현

---
+++ src/components/common/buttons/BaseButton.tsx
@@ -0,0 +1,36 @@
+'use Client';
+
+type BaseButtonProps = {
+  variant?: 'primary' | 'disabled' | 'outline' | 'soft' | 'ghost';
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+  type?: 'button' | 'submit' | 'reset';
+};
+
+const variantClasses = {
+  primary: 'bg-primary-300 text-white',
+  disabled: 'bg-gray-200 text-gray-500 cursor-not-allowed',
+  outline: 'border border-gray-200 text-gray-500 bg-white',
+  soft: 'bg-primary-100 text-primary-300',
+  ghost: 'text-gray-500',
+};
+
+export default function BaseButton({
+  variant = 'primary',
+  children,
+  onClick,
+  className = '',
+  type = 'button',
+}: BaseButtonProps) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={variant === 'disabled'}
+      className={`w-full px-4 py-3 text-center rounded-[10px] font-semibold ${variantClasses[variant]} ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
