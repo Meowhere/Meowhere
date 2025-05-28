8cb4fdf19b054e494fbbbf05dff8a1121ca38aa6
tansxx <159680008+tansxx@users.noreply.github.com>
2025-05-29 00:58:32 +0900

    📝 Fix: KebabButton으로 이름 변경

---
+++ src/components/common/buttons/KebabButton.tsx
@@ -0,0 +1,26 @@
+'use Client';
+
+type KebabButtonProps = {
+  onToggle: () => void;
+  className?: string;
+};
+
+export default function MeatballButton({
+  onToggle,
+  className = '',
+}: KebabButtonProps) {
+  return (
+    <button
+      type='button'
+      onClick={onToggle}
+      aria-label='옵션 열기'
+      className={className}
+    >
+      <img
+        src='/assets/icons/ico-kebab-menu.svg'
+        alt='더보기 아이콘'
+        className='w-10 h-10 object-contain'
+      />
+    </button>
+  );
+}
