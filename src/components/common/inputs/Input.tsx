ab60044dbd1720506e3e9b06ae73c0238295818e
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-05-30 04:15:34 +0900

    💄style: font 설정

--- src/components/common/inputs/Input.tsx
+++ src/components/common/inputs/Input.tsx
@@ -24,7 +24,7 @@ export default function Input({
       <div
         className={clsx(
           'flex items-center px-6 py-4 rounded-2xl border bg-white relative',
-          error ? 'border-red-300' : 'border-gray-200 focus-within:border-gray-200'
+          error ? 'border-red-300' : 'border-gray-200 focus-within:border-gray-200',
         )}
       >
         {/* Floating label */}
@@ -32,28 +32,28 @@ export default function Input({
           className={clsx(
             'absolute left-6 pointer-events-none select-none transition-all duration-200',
             shouldFloat
-              ? 'top-2 text-[1.0rem] font-regular text-gray-400'
-              : 'top-1/2 -translate-y-1/2 text-xs font-regular text-gray-400'
+              ? 'top-2 text-xs font-regular text-gray-400'
+              : 'top-1/2 -translate-y-1/2 text-md font-regular text-gray-400',
           )}
         >
           {label}
         </label>
         {/* input & icon */}
-        <div className='flex-1 flex items-center'>
+        <div className="flex-1 flex items-center">
           <input
             type={inputType}
             value={value}
             onChange={onChange}
             onFocus={() => setIsFocused(true)}
             onBlur={() => setIsFocused(false)}
-            className='w-full bg-transparent border-none focus:outline-none text-xs font-regular text-gray-800 pt-2'
-            autoComplete='off'
+            className="w-full bg-transparent border-none focus:outline-none text-md font-regular text-gray-800 pt-2"
+            autoComplete="off"
           />
           {isPassword && (
             <button
-              type='button'
+              type="button"
               tabIndex={-1}
-              className='ml-2'
+              className="ml-2"
               onClick={() => setVisible((v) => !v)}
             >
               <Image
@@ -71,7 +71,7 @@ export default function Input({
           )}
         </div>
       </div>
-      {error && <div className='text-red-300 text-[15px] mt-2 ml-2'>{error}</div>}
+      {error && <div className="text-red-300 text-sm mt-2 ml-2">{error}</div>}
     </div>
   );
 }
