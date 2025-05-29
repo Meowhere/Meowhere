507b1b7d539a605a53b141203ec1b3d488c1c081
tansxx <159680008+tansxx@users.noreply.github.com>
2025-05-29 12:54:05 +0900

    ✨ Feat: StarRating 컴포넌트 구현

---
+++ src/components/common/buttons/StarRating.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import StarButton from './StarButton';
+
+type StarRatingProps = {
+  value: number;
+  onChange?: (value: number) => void;
+  readOnly?: boolean;
+  className?: string;
+};
+
+export default function StarRating({
+  value,
+  onChange,
+  readOnly = false,
+  className = '',
+}: StarRatingProps) {
+  const handleClick = (index: number) => {
+    if (readOnly || !onChange) return;
+
+    if (value === index + 1) {
+      onChange(index);
+    } else {
+      onChange(index + 1);
+    }
+  };
+
+  return (
+    <div className='flex gap-1'>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <StarButton
+          key={index}
+          filled={index < value}
+          onClick={() => handleClick(index)}
+        />
+      ))}
+    </div>
+  );
+}
