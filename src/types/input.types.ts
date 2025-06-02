4eb17f17dc33c1778f52ca840bd6bd9ac5e3aeff
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-02 22:42:18 +0900

    ✨ feat: 모바일 구현 + 버튼

--- src/types/input.types.ts
+++ src/types/input.types.ts
@@ -3,7 +3,7 @@ import { ChangeEventHandler } from 'react';
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   type: 'text' | 'email' | 'password' | 'number';
-  onChange: ChangeEventHandler<HTMLInputElement>;
+  // onChange: ChangeEventHandler<HTMLInputElement>;
   error?: string;
   isPassword?: boolean;
 }
