744daa4778a04f9c48f4d493271868d464881efc
huiseong29 <tobestar29@naver.com>
2025-06-02 18:35:53 +0900

    ✨ Feat: 예약 내역 UI 구현

--- src/app/my-page/reservations/page.tsx
+++ src/app/my-page/reservations/page.tsx
@@ -4,34 +4,65 @@ import Image from 'next/image';
 import ReservationsCard from '../reservations/components/ReservationsCard';
 
 export default function ReservationsTestPage() {
-  const testData: any[] = [];
+  // const testData: any[] = [];
 
-  const hasData = testData.length > 0;
+  const testData = [
+    {
+      label: '예약 완료',
+      imageUrl: '/assets/icons/test_img.png',
+      title: '함께 배우면 즐거운 스트릿 댄스',
+      date: '2023. 2. 14',
+      time: '11:00 - 12:30',
+      headCount: 10,
+      price: 148000,
+      showCancel: true,
+    },
+    {
+      label: '예약 승인',
+      imageUrl: '/assets/icons/test_img.png',
+      title: '함께 배우면 즐거운 스트릿 댄스',
+      date: '2023. 2. 14',
+      time: '11:00 - 12:30',
+      headCount: 10,
+      price: 148000,
+    },
+    {
+      label: '예약 취소',
+      imageUrl: '/assets/icons/test_img.png',
+      title: '함께 배우면 즐거운 스트릿 댄스',
+      date: '2023. 2. 14',
+      time: '11:00 - 12:30',
+      headCount: 10,
+      price: 148000,
+    },
+    {
+      label: '예약 거절',
+      imageUrl: '/assets/icons/test_img.png',
+      title: '함께 배우면 즐거운 스트릿 댄스',
+      date: '2023. 2. 14',
+      time: '11:00 - 12:30',
+      headCount: 10,
+      price: 148000,
+    },
+    {
+      label: '체험 완료',
+      imageUrl: '/assets/icons/test_img.png',
+      title: '함께 배우면 즐거운 스트릿 댄스',
+      date: '2023. 2. 14',
+      time: '11:00 - 12:30',
+      headCount: 10,
+      price: 148000,
+      showReview: true,
+    },
+  ];
 
   return (
-    <main className='bg-gray-50 min-h-screen flex flex-col items-center justify-center px-[2rem] py-[2.4rem]'>
-      {hasData ? (
-        <div className='w-full max-w-[600px] flex flex-col gap-[2rem]'>
-          {testData.map((item, idx) => (
-            <ReservationsCard
-              key={idx}
-              {...item}
-              onCancel={() => alert('예약 취소 클릭')}
-              onReview={() => alert('후기 쓰기 클릭')}
-            />
-          ))}
-        </div>
-      ) : (
-        <div className='flex flex-col items-center justify-center'>
-          <Image
-            src='/assets/icons/logo/ico-empty-view-logo.svg'
-            alt='빈 상태 이미지'
-            width={82}
-            height={123}
-          />
-          <p className='text-md text-gray-500 mt-[2.4rem]'>예약한 체험이 없다냥</p>
-        </div>
-      )}
+    <main className='bg-gray-50 min-h-screen flex flex-col items-center pt-[4.4rem]'>
+      <div className='w-full max-w-[600px] flex flex-col h-[calc(100vh-13.6rem)]'>
+        {testData.map((item, idx) => (
+          <ReservationsCard key={idx} {...item} />
+        ))}
+      </div>
     </main>
   );
 }
