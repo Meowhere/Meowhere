3b8d67baed7450eb2f96c5bf87b1994ee00374f0
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-03 14:25:22 +0900

    💄style: svg 수정

--- src/types/profile-menu.types.ts
+++ src/types/profile-menu.types.ts
@@ -1,13 +1,13 @@
 export interface IconTypes {
   'my-info': string;
-  'my-reservation': string;
+  'my-reservations': string;
   'my-activities': string;
   settings: string;
 }
 export type IconType = keyof IconTypes;
 export const ICONS: IconTypes = {
   'my-info': '/assets/icons/account/account-people.svg',
-  'my-reservation': '/assets/icons/account/account-history.svg',
+  'my-reservations': '/assets/icons/account/account-history.svg',
   'my-activities': '/assets/icons/account/account-calendar.svg',
   settings: '/assets/icons/account/ico-settings.svg',
 };
