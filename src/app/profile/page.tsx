6d705bca715da52e94204ea35e6edf079e3bafaa
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-03 14:22:58 +0900

    ✅ test: 테스트 코드 작성

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,19 @@
 'use client';
-import ProfileItem from './components/ProfileItem';
+import ProfileMenuItem from './components/ProfileMenuItem';
 
 export default function Profile() {
-  return <ProfileItem href='/profile/my-info' icon='my-info' title='내 정보' />;
+  return (
+    <div>
+      <div>
+        <ProfileMenuItem href='/profile/my-info' icon='my-info' title='내 정보' />
+        <ProfileMenuItem href='/profile/my-reservations' icon='my-reservations' title='예약 내역' />
+        <ProfileMenuItem
+          href='/profile/my-activities'
+          icon='my-activities'
+          title='예약 요청 관리'
+        />
+        <ProfileMenuItem href='/profile/reservations' icon='settings' title='체험 수정하기' />
+      </div>
+    </div>
+  );
 }
