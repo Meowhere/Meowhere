bb40f43e1328c5680524816460e4da482069111c
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-03 21:35:29 +0900

    💄style: 이름 -이메일 부분 가운데 정렬

--- src/app/profile/components/ProfileItem.tsx
+++ src/app/profile/components/ProfileItem.tsx
@@ -12,7 +12,7 @@ export default function ProfileItem() {
           <UploadImg className='w-[36px] h-[36px]' alt='프로필 이미지 업로드' />
         </button>
       </div>
-      <div className='flex flex-col gap-[8px]'>
+      <div className='flex flex-col items-center justify-center gap-[8px]'>
         <h2 className='text-xl font-semibold text-gray-800'>사용자 이름</h2>
         <p className='text-lg font-regular text-gray-600'>사용자 이메일</p>
       </div>
