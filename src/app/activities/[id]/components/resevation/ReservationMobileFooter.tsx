5bc818ba3cf31f21f3ef040f188a120917555aa7
tansxx <159680008+tansxx@users.noreply.github.com>
2025-06-05 01:11:37 +0900

    ✨ Feat: 일정 선택 모바일 버전 구현

--- src/app/activities/[id]/components/resevation/ReservationMobileFooter.tsx
+++ src/app/activities/[id]/components/resevation/ReservationMobileFooter.tsx
@@ -12,8 +12,8 @@ export default function ReservationMobileFooter({
   onClickDateSelect,
 }: ReservationMobileFooterProps) {
   return (
-    <div className='fixed bottom-0 left-0 w-full px-4 pb-4 z-50 bg-transparent'>
-      <div className='w-full h-[64px] px-[8px] bg-white rounded-full border border-gray-200 shadow-[0px_4px_40px_0px_rgba(0,0,0,0.10)] backdrop-blur-md flex items-center'>
+    <div className='fixed flex justify-center bottom-[24px] left-0 w-full px-4 pb-4 z-50 bg-transparent'>
+      <div className='w-[327px] h-[64px] py-[16px] px-[8px] bg-white rounded-full border border-gray-200 shadow-[0px_4px_40px_0px_rgba(0,0,0,0.10)] backdrop-blur-md flex items-center'>
         {/* 좌측: 텍스트 */}
         <p className='text-lg font-semibold px-[24px] text-gray-800'>
           ₩ {pricePerPerson.toLocaleString()} <span>/ 인</span>
@@ -23,7 +23,7 @@ export default function ReservationMobileFooter({
         <div className='ml-auto'>
           <BaseButton
             onClick={onClickDateSelect}
-            className='w-[100px] h-[48px] text-lg font-bold rounded-full transition'
+            className='w-[106px] h-[48px] text-lg font-bold rounded-full transition'
           >
             일정 선택
           </BaseButton>
