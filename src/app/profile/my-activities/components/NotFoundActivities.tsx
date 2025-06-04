6024be5fc35720d481bf4d8931ab0c29fd058011
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-04 18:27:44 +0900

    ✨feat: 체험등록없는 경우 페이지 구현

--- src/app/profile/my-activities/components/NotFoundActivities.tsx
+++ src/app/profile/my-activities/components/NotFoundActivities.tsx
@@ -6,27 +6,35 @@ import Image from 'next/image';
 export default function NotFoundActivities() {
   const { isDesktop } = useBreakpoint();
   return (
-    <div className='flex flex-col items-center justify-center '>
+    <div className='flex flex-col items-center justify-center gap-6 lg:flex-row lg:gap-[128px]'>
       <Image
         src='/assets/icons/logo/ico-empty-view-logo.svg'
         alt='빈 상태 이미지'
         width={82}
         height={123}
+        style={{
+          width: isDesktop ? '282px' : '82px',
+          height: isDesktop ? '423px' : '123px',
+        }}
       />
-      <h1 className='text-2lg font-semibold text-gray-400'>등록된 체험이 없다냥</h1>
-      {isDesktop && (
-        <div className='flex flex-col items-center gap-[46px] py-[22px]'>
-          <p className='text-md font-regular text-gray-400'>
-            문화, 액티비티, 힐링 서비스를 둘러봐. <br /> 예약이 있으면 여기에 표시돼!
-          </p>
-          <BaseButton
-            variant='soft'
-            color='red'
-            children='체험 등록하러 가기'
-            className='w-[180px]'
-          />
-        </div>
-      )}
+      <div className='flex flex-col lg:items-start'>
+        <h1 className='text-2lg font-semibold text-gray-400 lg:text-left'>등록된 체험이 없다냥</h1>
+        {isDesktop && (
+          <div className='flex flex-col items-start gap-[46px] py-[22px]'>
+            <p className='text-md font-regular text-gray-400'>
+              문화, 액티비티, 힐링 서비스를 둘러봐. <br /> 예약이 있으면 여기에 표시돼!
+            </p>
+            <a href='/profile/my-activities/register'>
+              <BaseButton
+                variant='soft'
+                color='red'
+                children='체험 등록하러 가기'
+                className='w-[180px]'
+              />
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
