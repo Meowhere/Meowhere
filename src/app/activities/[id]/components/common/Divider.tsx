be47f809c1660247ef2edc55845c47b66454e630
tansxx <159680008+tansxx@users.noreply.github.com>
2025-06-04 23:40:17 +0900

    ✨ Feat: ReservationBox 구현

--- src/app/activities/[id]/components/common/Divider.tsx
+++ src/app/activities/[id]/components/common/Divider.tsx
@@ -1,3 +1,7 @@
 export default function Divider({ className = '' }: { className?: string }) {
-  return <hr className={`w-full border-t border-gray-200 my-6 ${className}`} />;
+  return (
+    <div className='px-[12px]'>
+      <hr className={`w-full border-t border-gray-200 my-6 ${className}`} />
+    </div>
+  );
 }
--- src/app/activities/[id]/components/resevation/ReservationMobileFooter.tsx
+++ src/app/activities/[id]/components/resevation/ReservationMobileFooter.tsx
@@ -13,16 +13,21 @@ export default function ReservationMobileFooter({
 }: ReservationMobileFooterProps) {
   return (
     <div className='fixed bottom-0 left-0 w-full px-4 pb-4 z-50 bg-transparent'>
-      <div className='w-full h-[64px] flex items-center justify-between gap-10 left-[24px] bg-white rounded-full border border-gray-200 shadow-[0px_4px_40px_0px_rgba(0,0,0,0.10)] backdrop-blur-md px-6 py-4'>
-        <p className='text-lg font-semibold text-gray-800'>
+      <div className='w-full h-[64px] px-[8px] bg-white rounded-full border border-gray-200 shadow-[0px_4px_40px_0px_rgba(0,0,0,0.10)] backdrop-blur-md flex items-center'>
+        {/* 좌측: 텍스트 */}
+        <p className='text-lg font-semibold px-[24px] text-gray-800'>
           ₩ {pricePerPerson.toLocaleString()} <span>/ 인</span>
         </p>
-        <BaseButton
-          onClick={onClickDateSelect}
-          className='w-[106px] h-[48px] text-lg font-bold rounded-full transition'
-        >
-          일정 선택
-        </BaseButton>
+
+        {/* 우측: 버튼 (오른쪽 끝으로 밀기 위해 margin-left 자동 설정) */}
+        <div className='ml-auto'>
+          <BaseButton
+            onClick={onClickDateSelect}
+            className='w-[100px] h-[48px] text-lg font-bold rounded-full transition'
+          >
+            일정 선택
+          </BaseButton>
+        </div>
       </div>
     </div>
   );
