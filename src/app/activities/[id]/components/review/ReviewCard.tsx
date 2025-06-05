0f9fae828508c0aafe618558e07eb5c0301b1301
tansxx <159680008+tansxx@users.noreply.github.com>
2025-06-06 01:20:35 +0900

    ✨ Feat: ReviewSection.tsx 컴포넌트 생성

--- src/app/activities/[id]/components/review/ReviewCard.tsx
+++ src/app/activities/[id]/components/review/ReviewCard.tsx
@@ -44,4 +44,3 @@ export default function ReviewCard({
     </div>
   );
 }
-s;
---
+++ src/app/activities/[id]/components/review/ReviewSection.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import ReviewCard from './ReviewCard';
+import ReviewSummary from './ReviewSummary';
+import { getSatisfactionLabel } from '@/src/utils/getSatisfactionLabel';
+import { Review } from '@/src/types/review.types';
+import VerticalDivider from '../common/VerticalDivider';
+
+interface ReviewSectionProps {
+  rating: number;
+  reviewCount: number;
+  reviews: Review[];
+  onOpenModal?: () => void;
+}
+
+export default function ReviewSection({
+  rating,
+  reviewCount,
+  reviews,
+  onOpenModal,
+}: ReviewSectionProps) {
+  const previewReviews = reviews.slice(0, 4);
+
+  return (
+    <section className='px-4 py-6'>
+      <div className='flex justify-center items-center'>
+        <ReviewSummary
+          rating={rating}
+          reviewCount={reviewCount}
+          satisfactionLabel={getSatisfactionLabel(rating)}
+        />
+      </div>
+
+      <div className='flex gap-4 overflow-x-auto pb-2 snap-x scroll-smooth scrollbar-hide'>
+        {previewReviews.map((review, idx) => (
+          <div key={review.id} className='flex items-stretch'>
+            <div className='snap-start shrink-0 w-[256px] mr-[12px]'>
+              <ReviewCard
+                nickname={review.user.nickname}
+                profileImageUrl={review.user.profileImageUrl}
+                createdAt={review.createdAt}
+                content={review.content}
+                rating={review.rating}
+              />
+            </div>
+
+            {idx !== previewReviews.length - 1 && <VerticalDivider className='ml-[12px]' />}
+          </div>
+        ))}
+      </div>
+
+      <button
+        className='mt-[24px] w-full px-0 py-[12px] text-sm text-gray-800 bg-gray-100 rounded-[14px] text-center'
+        onClick={onOpenModal}
+      >
+        모든 후기 보기
+      </button>
+    </section>
+  );
+}
