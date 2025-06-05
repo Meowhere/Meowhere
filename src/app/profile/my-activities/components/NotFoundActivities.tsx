eb0352d8b6017d58f442a44f5b43b62be6219267
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-05 16:03:23 +0900

    🐛fix: basebutton 처리 완료

--- src/app/profile/my-activities/components/NotFoundActivities.tsx
+++ src/app/profile/my-activities/components/NotFoundActivities.tsx
@@ -2,9 +2,11 @@
 import BaseButton from '@/src/components/common/buttons/BaseButton';
 import { useBreakpoint } from '@/src/hooks/useBreakpoint';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function NotFoundActivities() {
   const { isDesktop } = useBreakpoint();
+  const router = useRouter();
   return (
     <div className='flex flex-col items-center justify-center gap-6 lg:flex-row lg:gap-[128px]'>
       <Image
@@ -20,14 +22,15 @@ export default function NotFoundActivities() {
             <p className='text-md font-regular text-gray-400'>
               문화, 액티비티, 힐링 서비스를 둘러봐. <br /> 예약이 있으면 여기에 표시돼!
             </p>
-            <a href='/profile/my-activities/register'>
-              <BaseButton
-                variant='soft'
-                color='red'
-                children='체험 등록하러 가기'
-                className='w-[180px]'
-              />
-            </a>
+            <BaseButton
+              variant='soft'
+              color='red'
+              children='체험 등록하러 가기'
+              className='w-[180px]'
+              onClick={() => {
+                router.push('/profile/my-activities/register');
+              }}
+            />
           </div>
         )}
       </div>
