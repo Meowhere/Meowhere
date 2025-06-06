38ceaeac331e8eadb8ed8e908edd7782ad218a2b
numi8462 <yhk8462@naver.com>
2025-06-06 14:31:17 +0900

    refactor: checkEmailExistance 함수 수정

--- src/utils/checkEmailExistence.ts
+++ src/utils/checkEmailExistence.ts
@@ -2,19 +2,29 @@ import { BASE_API_URL } from '../constants/api';
 
 export async function checkEmailExistence(email: string): Promise<boolean | null> {
   const credentials = {
-    email: email,
+    email,
     password: '1',
   };
 
-  const res = await fetch(`${BASE_API_URL}/auth/login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(credentials),
-  });
+  try {
+    const res = await fetch(`${BASE_API_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    });
 
-  if (res.status === 400) return true;
-  if (res.status === 404) return false;
-  return null;
+    switch (res.status) {
+      case 400:
+        return true; // 이메일 존재
+      case 404:
+        return false; // 이메일 없음
+      default:
+        return null; // 예외 상황
+    }
+  } catch (error) {
+    console.error('checkEmailExistence error:', error);
+    return null;
+  }
 }
