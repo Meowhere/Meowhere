76bd467c16d2af71aaacc8edad645b792631079e
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-07 00:00:18 +0900

    🐛fix: register type 변경

--- src/types/input.types.ts
+++ src/types/input.types.ts
@@ -1,17 +1,16 @@
-import { ChangeEventHandler } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   type: 'text' | 'email' | 'password' | 'number';
-  // onChange: ChangeEventHandler<HTMLInputElement>;
   error?: string;
   isPassword?: boolean;
-  register?: any; // For react-hook-form
+  register?: UseFormRegisterReturn; // For react-hook-form
 }
 export interface TextareaProps {
   value?: string;
   error?: string;
   className?: string;
-  register?: any; // For react-hook-form
+  register?: UseFormRegisterReturn; // For react-hook-form
   placeholder?: string;
 }
