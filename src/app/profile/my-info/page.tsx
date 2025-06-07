67c5f0a5c4d3f0148dd5d6e3ac8391e6d4da6282
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-07 22:11:31 +0900

    ✨feat: 닉네임 비밀번호 유효성 검사 완료

--- src/app/profile/my-info/page.tsx
+++ src/app/profile/my-info/page.tsx
@@ -1,71 +1,86 @@
 'use client';
+import { useForm } from 'react-hook-form';
 import Input from '@/src/components/common/inputs/Input';
 import BaseButton from '@/src/components/common/buttons/BaseButton';
 import { useBreakpoint } from '@/src/hooks/useBreakpoint';
+import { error } from 'console';
+
+type FormValues = {
+  nickname: string;
+  password: string;
+  confirmPassword: string;
+};
 
 export default function MyInfoPage() {
   const { isDesktop } = useBreakpoint();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormValues>({
+    mode: 'onChange',
+  });
 
-  // const [pw, setPw] = useState('');
-  // const [pwError, setPwError] = useState('');
-  // const [pwConfirm, setPwConfirm] = useState('');
-  // const [pwConfirmError, setPwConfirmError] = useState('');
-  // const [nickname, setNickname] = useState('');
-  // const [nicknameError, setNicknameError] = useState('');
+  const nicknameValue = watch('nickname', '');
+  const pwValue = watch('password', '');
+  const pwConfirmValue = watch('confirmPassword', '');
 
-  // // 비밀번호 유효성 검사
-  // const handlePwChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const val = e.target.value;
-  //   setPw(val);
-  //   setPwError(val.length > 0 && val.length < 6 ? '6자 이상 입력하세요' : '');
-  // }, []);
-
-  // // 비밀번호 확인 유효성 검사
-  // const handlePwConfirmChange = useCallback(
-  //   (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     const val = e.target.value;
-  //     setPwConfirm(val);
-  //     setPwConfirmError(val !== pw ? '비밀번호가 일치하지 않습니다' : '');
-  //   },
-  //   [pw]
-  // );
-
-  // // 닉네임 유효성 검사
-  // const handleNicknameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const val = e.target.value;
-  //   setNickname(val);
-  //   setNicknameError(val.length > 0 && val.length < 2 ? '2자 이상 입력하세요' : '');
-  // }, []);
+  const onSubmit = (data: FormValues) => {
+    alert(JSON.stringify(data, null, 2));
+  };
 
   return (
-    <div className='flex flex-col gap-[64px] mt-[48px] mx-[24px] mb-[600px]'>
+    <form
+      className='flex flex-col gap-[64px] mt-[48px] mx-[24px] mb-[600px]'
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className='flex flex-col gap-[16px]'>
         <p className='text-xl font-semibold text-gray-800 '>닉네임 변경</p>
         <Input
+          name='nickname'
           label='닉네임'
           type='text'
-          // value={nickname}
-          // onChange={handleNicknameChange}
-          // error={nicknameError}
+          register={register('nickname', {
+            required: '닉네임을 입력해 주세요.',
+            minLength: {
+              value: 2,
+              message: '2자 이상 입력하세요',
+            },
+            maxLength: {
+              value: 20,
+              message: '20자 이하로 입력하세요',
+            },
+          })}
+          value={nicknameValue}
+          error={errors.nickname?.message}
         />
       </div>
       <div className='flex flex-col gap-[16px]'>
         <p className='text-xl font-semibold text-gray-800'>비밀번호 변경</p>
         <div>
           <Input
+            name='password'
             label='비밀번호'
             type='password'
-            // value={pw}
-            // onChange={handlePwChange}
-            // error={pwError}
+            register={register('password', {
+              required: '비밀번호를 입력해 주세요',
+              minLength: { value: 6, message: '6자 이상 입력하세요' },
+            })}
+            value={pwValue}
+            error={errors.password?.message}
             isPassword={true}
           />
           <Input
+            name='confirmPassword'
             label='비밀번호 확인'
             type='password'
-            // value={pwConfirm}
-            // onChange={handlePwConfirmChange}
-            // error={pwConfirmError}
+            register={register('confirmPassword', {
+              required: '비밀번호를 확인해 주세요',
+              validate: (value) => value === pwValue || '비밀번호가 일치하지 않습니다.',
+            })}
+            value={pwConfirmValue}
+            error={errors.confirmPassword?.message}
             isPassword={true}
           />
         </div>
@@ -73,10 +88,12 @@ export default function MyInfoPage() {
       {isDesktop && (
         <div className='flex justify-end w-full'>
           <div className='w-[128px]'>
-            <BaseButton disabled={false}>변경하기</BaseButton>
+            <BaseButton variant='primary' disabled={false}>
+              변경하기
+            </BaseButton>
           </div>
-        </div> // 데스크탑일 때는 버튼 활성화
+        </div>
       )}
-    </div>
+    </form>
   );
 }
