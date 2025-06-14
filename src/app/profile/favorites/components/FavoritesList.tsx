254a062e9da3e363d8a56acba64e3c2f52e53c78
numi8462 <yhk8462@naver.com>
2025-06-14 17:17:18 +0900

    feat: 찜목록 UI 구현

---
+++ src/app/profile/favorites/components/FavoritesList.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { useFavoritesStore } from '@/src/store/favoritesStore';
+import FavoriteCard from './FavoriteCard';
+import { useState } from 'react';
+import { Activity } from '@/src/types/activity.types';
+import NotFoundFavorites from './NotFoundActivities';
+
+export default function FavoritesList() {
+  const { favorites } = useFavoritesStore();
+  const [myFavorites] = useState<Activity[]>(favorites);
+
+  if (myFavorites.length === 0) return <NotFoundFavorites />;
+
+  return (
+    <div className='flex flex-col w-full lg:mt-[64px]'>
+      {myFavorites.map((fav) => (
+        <FavoriteCard activity={fav} />
+      ))}
+    </div>
+  );
+}
