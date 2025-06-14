f08a4fd2e14598c5835662961ad8f6bef84e2724
numi8462 <yhk8462@naver.com>
2025-06-14 17:17:35 +0900

    feat: 찜목록 페이지 및 찜없음 추가

--- src/app/profile/favorites/page.tsx
+++ src/app/profile/favorites/page.tsx
@@ -1,3 +1,9 @@
+import FavoritesList from './components/FavoritesList';
+
 export default function FavoritesPage() {
-  return <div></div>;
+  return (
+    <div className='relative flex flex-col mx-[24px] my-[112px]'>
+      <FavoritesList />
+    </div>
+  );
 }
