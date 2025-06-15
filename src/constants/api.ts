c7dccc3d8da9da84aa865d4b9433fd0b3a117d9e
numi8462 <yhk8462@naver.com>
2025-06-15 15:45:12 +0900

    feat: 카카오 앱키 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,3 +2,4 @@ export const BASE_URL = process.env.NEXT_PUBLIC_APP_URL;
 export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 export const KAKAO_API = process.env.NEXT_PUBLIC_KAKAO_API;
 export const KAKAO_MAP_API_KEY = process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY;
+export const KAKAO_APP_KEY = process.env.NEXT_PUBLIC_KAKAO_APP_KEY;
