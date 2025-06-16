dae6f838e4d25b0a462891febd476b716b521a7d
numi8462 <yhk8462@naver.com>
2025-06-16 22:16:54 +0900

    feat: gnb 적용

--- src/app/profile/favorites/page.tsx
+++ src/app/profile/favorites/page.tsx
@@ -1,6 +1,11 @@
+'use client';
+import { useGnb } from '@/src/hooks/useGnb';
 import FavoritesList from './components/FavoritesList';
+import { useRouter } from 'next/navigation';
 
 export default function FavoritesPage() {
+  const router = useRouter();
+  useGnb({ title: '찜목록', backAction: () => router.back() });
   return (
     <div className='relative flex flex-col mx-[24px]'>
       <FavoritesList />
