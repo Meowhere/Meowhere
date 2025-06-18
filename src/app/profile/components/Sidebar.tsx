16ae7d602b3a388fe52db92fc4565a4cdd13b972
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-06-18 22:03:44 +0900

    ✨Feat: 내 체험 통계 메뉴 추가

--- src/app/profile/components/Sidebar.tsx
+++ src/app/profile/components/Sidebar.tsx
@@ -24,9 +24,14 @@ export default function Sidebar() {
       <div className='flex flex-col gap-[8px]'>
         <ProfileMenuItem href='/profile/my-info' icon='my-info' title='내 정보' />
         <ProfileMenuItem href='/profile/favorites' icon='favorites' title='찜 목록' />
-        <ProfileMenuItem href='/profile/my-reservations' icon='reservations' title='예약 내역' />
+        <ProfileMenuItem href='/profile/reservations' icon='reservations' title='예약 내역' />
         <ProfileMenuItem href='/profile/my-activities' icon='my-activities' title='내 체험 관리' />
-        <ProfileMenuItem href='/profile/reservations' icon='settings' title='내 체험 예약 관리' />
+        <ProfileMenuItem
+          href='/profile/my-reservations'
+          icon='my-reservation'
+          title='내 체험 예약 관리'
+        />
+        <ProfileMenuItem href='/profile/analytics' icon='analytics' title='내 체험 통계' />
         {!isDesktop && (
           <>
             {theme === 'dark' ? (
