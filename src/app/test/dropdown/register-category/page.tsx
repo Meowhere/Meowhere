deea8178c8fc17190f5f8a3963725d1d70af2ebe
solmin <smin2020@icloud.com>
2025-06-19 16:34:27 +0900

    🐛Fix: delete test page

--- src/app/test/dropdown/register-category/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterCategory from '@/src/app/profile/my-activities/components/register-form/RegisterCategory';
-
-export default function RegisterCategoryPage() {
-  return (
-    <div>
-      <RegisterCategory />
-    </div>
-  );
-}
