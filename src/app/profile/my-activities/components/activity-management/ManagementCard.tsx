dc3b769e5bca1bd4c9f7dc2319d6d86f80a6414c
numi8462 <yhk8462@naver.com>
2025-06-21 23:35:38 +0900

    refactor: 버블링 수정

--- src/app/profile/my-activities/components/activity-management/ManagementCard.tsx
+++ src/app/profile/my-activities/components/activity-management/ManagementCard.tsx
@@ -23,8 +23,8 @@ export default function ManagementCard({
 
   return (
     <div className='flex justify-between border-b border-gray-200 dark:border-gray-700 last:border-b-0 py-[24px] w-full'>
-      <Link href={`/activities/${id}`}>
-        <div className='flex flex-row items-center justify-center gap-[10px] lg:gap-[14px] cursor-pointer'>
+      <Link href={`/activities/${id}`} className='flex-1'>
+        <div className='flex flex-row items-center justify-start gap-[10px] lg:gap-[14px] cursor-pointer'>
           <Image
             src={bannerImageUrl}
             alt={title}
