23ac75716cd90530f630fc9f6596d465e873e4c2
tansxx <159680008+tansxx@users.noreply.github.com>
2025-06-21 22:15:29 +0900

    🐛 fix: 타입 props로 빼기

--- src/app/activities/[id]/components/ExperienceDetailClient.tsx
+++ src/app/activities/[id]/components/ExperienceDetailClient.tsx
@@ -4,18 +4,19 @@ import ExperienceResponsiveLayout from './ExperienceResponsiveLayout';
 import { Activity } from '@/src/types/activity.types';
 import { ScheduleWithTimes } from '@/src/types/schedule.types';
 import { Review } from '@/src/types/review.type';
+export interface ExperienceDetailClientProps {
+  activity: Activity; // 체험 상세 정보
+  schedules: ScheduleWithTimes[]; // 예약 가능 스케줄
+  reviews: Review[]; // 체험 리뷰 목록
+  reviewStats: { totalCount: number; averageRating: number }; // 리뷰 통계
+}
 
 export default function ExperienceDetailClient({
   activity,
   schedules,
   reviews,
   reviewStats,
-}: {
-  activity: Activity;
-  schedules: ScheduleWithTimes[];
-  reviews: Review[];
-  reviewStats: { totalCount: number; averageRating: number };
-}) {
+}: ExperienceDetailClientProps) {
   return (
     <ExperienceResponsiveLayout
       activity={activity}
