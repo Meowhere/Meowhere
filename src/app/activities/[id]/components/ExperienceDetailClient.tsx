8269deb657ea52fe0fcb66b4f7f4ba7b9fef74b3
tansxx <159680008+tansxx@users.noreply.github.com>
2025-06-21 21:50:34 +0900

    🐛 fix: 상세페이지 SSR 수정

--- src/app/activities/[id]/components/ExperienceDetailClient.tsx
+++ src/app/activities/[id]/components/ExperienceDetailClient.tsx
@@ -2,16 +2,28 @@
 
 import ExperienceResponsiveLayout from './ExperienceResponsiveLayout';
 import { Activity } from '@/src/types/activity.types';
+import { ScheduleWithTimes } from '@/src/types/schedule.types';
+import { Review } from '@/src/types/review.type';
 
-export default function DetailClient({ activity }: { activity: Activity }) {
+export default function ExperienceDetailClient({
+  activity,
+  schedules,
+  reviews,
+  reviewStats,
+}: {
+  activity: Activity;
+  schedules: ScheduleWithTimes[];
+  reviews: Review[];
+  reviewStats: { totalCount: number; averageRating: number };
+}) {
   return (
     <ExperienceResponsiveLayout
       activity={activity}
-      schedules={[]}
-      reviews={[]}
+      schedules={schedules}
+      reviews={reviews}
       reviewStats={{
-        rating: activity.rating || 0,
-        count: activity.reviewCount || 0,
+        rating: reviewStats.averageRating || 0,
+        count: reviewStats.totalCount || 0,
       }}
     />
   );
