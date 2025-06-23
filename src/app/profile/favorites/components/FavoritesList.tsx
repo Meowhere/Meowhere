11428602efd020a4089f3f25ce9f6732c3326793
numi8462 <yhk8462@naver.com>
2025-06-23 12:50:03 +0900

    fix: 찜목록 새로고침 수정

--- src/app/profile/favorites/components/FavoritesList.tsx
+++ src/app/profile/favorites/components/FavoritesList.tsx
@@ -6,19 +6,18 @@ import { Activity } from '@/src/types/activity.types';
 import NoActivities from '../../components/NoActivities';
 
 export default function FavoritesList() {
-  const { favorites } = useFavoritesStore();
-  const [myFavorites] = useState<Activity[]>(favorites);
+  const favorites = useFavoritesStore((state) => state.favorites);
 
-  if (myFavorites.length === 0)
+  if (favorites.length === 0)
     return <NoActivities title='좋아요한' urlPath='/' buttonTitle='체험 보러가기' />;
 
   return (
     <div className='flex flex-col w-full gap-[24px]'>
-      {myFavorites.map((fav, index) => (
+      {favorites.map((fav, index) => (
         <div
           key={fav.id}
           className={
-            index < myFavorites.length - 1 ? 'border-b border-gray-200 dark:border-gray-600' : ''
+            index < favorites.length - 1 ? 'border-b border-gray-200 dark:border-gray-600' : ''
           }
         >
           <FavoriteCard activity={fav} />
