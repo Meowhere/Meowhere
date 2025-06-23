fff58e0e04633a68b1bf7e3664e5d4eb2b1300ab
solmin <smin2020@icloud.com>
2025-06-23 23:36:01 +0900

    🐛Fix: delete test folders

--- src/app/test/skeleton/page.tsx
+++
@@ -1,5 +0,0 @@
-import SkeletonRegisterForm from '@/src/app/profile/my-activities/components/skeleton-ui/SkeletonRegisterForm';
-
-export default function Skeleton() {
-  return <SkeletonRegisterForm />;
-}
